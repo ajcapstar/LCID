3c1901d3d858470cc489db966118c964bfc689ff
Cod-ak <akpemekelly02@gmail.com>
2026-02-03 00:38:37 +0100

    save WIP

--- app/components/StickyCards.jsx
+++ app/components/StickyCards.jsx
@@ -3,37 +3,37 @@ import "./StickyCards.css";
 const StickyCards = () => {
   const stickyCardsData = [
     {
-      id: 1,
+      index: 1,
       title: "Modularity",
-      image: "/sticky-cards/card_1",
+      image: "/sticky-cards/card_1.jpg",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nostru possun possum possum possum possum possum possum possum possum",
     },
     {
-      id: 2,
+      index: 2,
       title: "Character",
-      image: "/sticky-cards/card_1",
+      image: "/sticky-cards/card_1.jpg",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nostru possun possum possum possum possum possum possum possum possum",
     },
     {
-      id: 3,
+      index: 3,
       title: "Character",
-      image: "/sticky-cards/card_1",
+      image: "/sticky-cards/card_1.jpg",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nostru possun possum possum possum possum possum possum possum possum",
     },
     {
-      id: 4,
+      index: 4,
       title: "Character",
-      image: "/sticky-cards/card_1",
+      image: "/sticky-cards/card_1.jpg",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nostru possun possum possum possum possum possum possum possum possum",
     },
     {
-      id: 5,
+      index: 5,
       title: "Character",
-      image: "/sticky-cards/card_1",
+      image: "/sticky-cards/card_1.jpg",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum nostru possun possum possum possum possum possum possum possum possum",
     },
@@ -41,16 +41,16 @@ const StickyCards = () => {
   return (
     <div className="sticky-cards">
       {stickyCardsData.map((cardData, id) => (
-        <div className="sticky-card" key={id}>
-          <div className="sticky-card-index" key={id}>
+        <div className="sticky-card" key={index}>
+          <div className="sticky-card-index" >
             <h1>{cardData.id}</h1>
           </div>
-          <div className="sticky-card-content" key={id}>
+          <div className="sticky-card-content" key={index}>
             <div className="sticky-card-content-wrapper">
               <div className="sticky-card-header">
                 <h1> {cardData.title}</h1>
                 <div className="sticky-card-image">
-                <img src={cardData.image} alt="" />
+                <Image src={cardData.image} alt="" width={500} height={500} />
                 </div>
                 <div className="sticky-card-copy">
                   <div className="sticky-card-copy-title">
