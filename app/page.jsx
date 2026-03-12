59484980b1f2f188254acd4d14329adbece74395
Cod-ak <akpemekelly02@gmail.com>
2026-03-12 09:51:11 +0100

    nothing

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
 "use client";
-
+import Nav from "./components/nav/nav";
 export default function Home() {
   return (
     <>
       <h1>starting something new with the nav and shii</h1>
+      <Nav />
     </>
   );
 }
